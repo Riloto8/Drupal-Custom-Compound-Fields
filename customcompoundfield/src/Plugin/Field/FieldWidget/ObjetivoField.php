3f946a5b0d920262c814565dd1827b269ec8f224
davidjguru <davidjguru@gmail.com>
2018-05-07 14:59:56 +0200

    Add new Widget for the ObjetivoType custom compound field

---
+++ customcompoundfield/src/Plugin/Field/FieldWidget/ObjetivoField.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\customcompoundfield\Plugin\Field\FieldWidget;
+
+use Drupal;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'ObjetivoWidget' widget.
+ *
+ * @FieldWidget(
+ *   id = "ObjetivoWidget",
+ *   label = @Translation("Cargar Objetivo"),
+ *   field_types = {
+ *     "Objetivo"
+ *   }
+ * )
+ */
+class ObjetivoWidget extends WidgetBase {
+	
+  /**
+   * Define the form for the field type.
+   * 
+   * Inside this method we can define the form used to edit the field type.
+   * 
+   * Here there is a list of allowed element types: https://goo.gl/XVd4tA
+   */
+	
+ 
+public function formElement(
+    FieldItemListInterface $items,
+    $delta, 
+    Array $element, 
+    Array &$form, 
+    FormStateInterface $formState
+  ) {
+    // Objetivo
+    $element['objetivo'] = [
+      '#type' => 'select',
+      '#title' => t('Objetivo'),
+      // Set here the current value for this field, or a default value (or 
+      // null) if there is no a value
+      '#default_value' => isset($items[$delta]->objetivo) ? 
+          $items[$delta]->objetivo : 1,
+      '#placeholder' => t('Objetivo'),
+    ];
+
+    // Resultado
+    $element['resultado'] = [
+      '#type' => 'textfield',
+      '#title' => t('Resultado'),
+      '#default_value' => isset($items[$delta]->resultado) ? 
+          $items[$delta]->resultado : "Introduzca aqui un resultado observado",
+      '#placeholder' => t('Resultado'),
+    ];
+
+   // Indicador
+    $element['indicador'] = [
+      '#type' => 'textfield',
+      '#title' => t('Indicador'),
+      '#default_value' => isset($items[$delta]->indicador) ?
+          $items[$delta]->indicador : "Introduzca aqui un indicador observado",
+      '#placeholder' => t('Indicador'),
+    ];
+
+
+    return $element;
+  }
+}
